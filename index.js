7b24170b21c315ee5b91c2916e6070d49b3aba19
Saimon Davi <saimon.dsfg@gmail.com>
2021-12-17 18:48:33 -0300

    Exercício cálculo saldo usando array, for e função

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-let nota = 40
-
-function getNota(notaNum) {
-    let notaLet = ''
-
-    let notaA = notaNum >= 90 && notaNum <= 100
-    let notaB = notaNum >= 80 && notaNum <= 89
-    let notaC = notaNum >= 70 && notaNum <= 79
-    let notaD = notaNum >= 60 && notaNum <= 69
-    let notaF = notaNum < 60 && notaNum >=0
-    
-    if(notaA) {
-        notaLet = 'A'
-    } else if(notaB) {
-        notaLet = 'B'
-    } else if(notaC) {
-        notaLet = 'C'
-    } else if(notaD) {
-        notaLet = 'D'
-    } else if(notaF) {
-        notaLet = 'F'
-    } else {
-        notaLet = 'Nota inválida'
-    }
-
-    return notaLet;
+let controleDeGastos = {
+    receitas: [ 2500, 3200, 250.43, 360.45],
+    despesas: [ 320.34, 128.45, 176.87, 1450.00]
 }
 
-console.log(getNota(nota))
+function somaArray(array) {
+    let somaArray = 0
+
+    for(let i of array) {
+        somaArray += i
+    }
+
+    return somaArray;
+}
+
+function calculoDispesas() {
+    let saldoPositivo = somaArray(controleDeGastos.receitas);
+    let saldoNegativo = somaArray(controleDeGastos.despesas);
+
+    return saldoPositivo - saldoNegativo;
+}
+
+console.log(calculoDispesas() + 'R$')

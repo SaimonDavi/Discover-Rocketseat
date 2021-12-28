f53ba78422629364ccbf10684c00ad99a2986f80
Saimon Davi <saimon.dsfg@gmail.com>
2021-12-28 19:23:33 -0300

    Exemplo das funções de 'escuta' de eventos

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
 // Para executá-lo esteja no diretório onde está o arquivo e execute o comando 'node nomeDoArquivo.exemplo'
 
-const events = require('events')
 const { EventEmitter } = require('events')
-
-console.log(events)
-console.log(EventEmitter)
 
 const ev = new EventEmitter()
 
-console.log(ev)
+ev.on('saySomething', (a) => {
+    console.log(`${a} listened`)
+})
+
+ev.once('saySomething', (a) => {
+    console.log(`${a} listened`)
+})
+
+ev.emit('saySomething', 'first message')
+ev.emit('saySomething', 'second message')

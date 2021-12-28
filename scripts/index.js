0f9f54bd79f7782704639b6a5f813e10ae831066
Saimon Davi <saimon.dsfg@gmail.com>
2021-12-28 18:27:13 -0300

    Exemplo da função clearTimeout

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 // Neste exemplo foi criado um timer no Node
 // Para executá-lo esteja no diretório onde está o arquivo e execute o comando 'node nomeDoArquivo.exemplo'
 
-const timeOut = 3000
-const finished = () => console.log('done!')
+const timeOut1 = 3000
+const finished1 = () => {
+    console.log('first done')
+    clearTimeout(secondTimer)
+}
+const timeOut2 = 6000
+const finished2 = () => console.log('second done')
 
-setTimeout(finished, timeOut)
-console.log('wait...')
+let firstTimer = setTimeout(finished1, timeOut1)
+let secondTimer = setTimeout(finished2, timeOut2)

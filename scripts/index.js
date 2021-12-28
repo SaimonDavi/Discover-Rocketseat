e85b9b85456ccc6ebeefb68285bf7cced49b1ee0
Saimon Davi <saimon.dsfg@gmail.com>
2021-12-28 18:39:40 -0300

    Exemplo das funções setInterval e clearInterval

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,9 @@
 // Neste exemplo foi criado um timer no Node
 // Para executá-lo esteja no diretório onde está o arquivo e execute o comando 'node nomeDoArquivo.exemplo'
 
-const timeOut1 = 3000
-const finished1 = () => {
-    console.log('first done')
-    clearTimeout(secondTimer)
-}
-const timeOut2 = 6000
-const finished2 = () => console.log('second done')
+const timeOut = 500
+const checking = () => console.log('checking!')
 
-let firstTimer = setTimeout(finished1, timeOut1)
-let secondTimer = setTimeout(finished2, timeOut2)
+let interval = setInterval(checking, timeOut)
+
+setTimeout(() => clearInterval(interval), 3000)

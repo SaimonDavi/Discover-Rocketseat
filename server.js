24297ec9e597ee5df3068890f41d262ed5074836
Saimon Davi <saimon.dsfg@gmail.com>
2022-02-13 23:16:42 -0300

    Utilizando o ForEach para renderizar parametros

--- server.js
+++ server.js
@@ -4,7 +4,31 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("pages/index");
+    const items = [
+        {
+            title: "Primeiro",
+            message: "Tarefa 1"
+        },
+        {
+            title: "Segundo",
+            message: "Tarefa 2"
+        },
+        {
+            title: "Terceiro",
+            message: "Tarefa 3"
+        },
+        {
+            title: "Quarto",
+            message: "Tarefa 4"
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML"
+
+    res.render("pages/index", {
+        messagesHome: items,
+        subtitle: subtitle
+    });
 });
 
 app.get("/about", function(req, res){

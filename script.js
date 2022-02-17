68baad7ecbdd71b33244d78e607867d2db561a6d
Saimon Davi <saimon.dsfg@gmail.com>
2022-02-16 23:01:22 -0300

    Exemplo de FIFO

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
-class Stack {
-    constructor(){
+class Queue {
+    constructor() {
         this.data = [];
-        this.top = -1;
     }
 
-    push(value){
-        this.top++;
-        this.data[this.top] = value;
-        return this.data;
+    enqueue(item) {
+        this.data.push(item);
+        console.log(`${item} chegou na fila`);
     }
 
-    pop(){
-        if(this.top < 0 ) return undefined;
-        const poppedTop = this.data[this.top];
-        delete this.data[this.top];
-        this.top--;
-        return poppedTop;
-    }
-
-    peek(){
-        return this.top >= 0 ? this.data[this.top] : undefined;
+    dequeue() {
+        const item = this.data.shift();
+        console.log(`${item} saiu da fila`);
     }
 }
 
-const stack = new Stack();
+const fila = new Queue();
 
 //adicionando
-stack.push('item 1');
-stack.push('item 2');
-console.log(stack.push('item 3'));
-
-console.log(stack.peek());
+fila.enqueue('Primeiro');
+fila.enqueue('Segundo');
+fila.enqueue('Terceiro');
 
 //removendo
-stack.pop();
-console.log(stack.pop());
-
-console.log(stack.pop());
+fila.dequeue();
+fila.dequeue();
+fila.dequeue();

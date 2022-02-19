70d77f60dbe801f50cc31c2c56940b734b736813
Saimon Davi <saimon.dsfg@gmail.com>
2022-02-19 19:06:37 -0300

    Exemplo de Encapsulamento

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-class Queue {
-    constructor() {
-        this.data = [];
+class Poligono {
+    constructor(altura, largura){
+        this.altura = altura;
+        this.largura = largura;
     }
 
-    enqueue(item) {
-        this.data.push(item);
-        console.log(`${item} chegou na fila`);
+    get area(){
+        return this.#calcularArea()
     }
 
-    dequeue() {
-        const item = this.data.shift();
-        console.log(`${item} saiu da fila`);
+    #calcularArea() {
+        return this.altura * this.largura;
     }
 }
 
-const fila = new Queue();
-
-//adicionando
-fila.enqueue('Primeiro');
-fila.enqueue('Segundo');
-fila.enqueue('Terceiro');
-
-//removendo
-fila.dequeue();
-fila.dequeue();
-fila.dequeue();
+let poligono = new Poligono(50, 60);
+console.log(poligono);
+console.log(poligono.area);
+console.log(poligono.#calcularArea());

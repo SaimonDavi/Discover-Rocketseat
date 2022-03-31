e915f7e8f32240c4a2349ff37da19111ad7237b5
Saimon Davi <saimon.dsfg@gmail.com>
2022-03-30 21:40:26 -0300

    Adicionando uma callback para filtrar as informações

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,6 @@
 import fetch from 'node-fetch';
 
-fetch("https://api.github.com/users/SaimonDavi").then((response) =>
-  console.log(response)
-);
-
-// const response = await fetch('https://api.github.com/users/SaimonDavi');
-// const body = await response.text();
-
-// console.log(body);
+fetch("https://api.github.com/users/SaimonDavi")
+    .then(response => {
+        response.json().then(data => console.log(data))
+    });

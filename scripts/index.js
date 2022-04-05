7ab797310e1f966a186ffb8220886c7dd33cc18c
Saimon Davi <saimon.dsfg@gmail.com>
2022-04-05 17:09:21 -0300

    Encadeando de maneira mais ordenada e adicionando uma mesnsagem de erro

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,8 @@
 import fetch from 'node-fetch';
 
 fetch("https://api.github.com/users/SaimonDavi")
-    .then(response => {
-        response.json()
-            .then(data => {
-                fetch(data.repos_url)
-                    .then(res => {
-                        res.json()
-                            .then(d => console.log(d))
-                    })
-            })
-    });
+.then( response => response.json())
+.then( data => fetch(data.repos_url))
+.then( res => res.json())
+.then( d => console.log(d))
+.catch( err => console.log(err))

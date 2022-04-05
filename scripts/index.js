ca542ac9cf9d2c449224770b7363d81522500562
Saimon Davi <saimon.dsfg@gmail.com>
2022-04-05 16:31:20 -0300

    Encadeando Promises

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,12 @@ import fetch from 'node-fetch';
 
 fetch("https://api.github.com/users/SaimonDavi")
     .then(response => {
-        response.json().then(data => console.log(data))
+        response.json()
+            .then(data => {
+                fetch(data.repos_url)
+                    .then(res => {
+                        res.json()
+                            .then(d => console.log(d))
+                    })
+            })
     });

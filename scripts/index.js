74cb51035a00438cf9b8594bd3afa3a5d67fc5b7
Saimon Davi <saimon.dsfg@gmail.com>
2022-04-06 15:05:59 -0300

    Utilizando o Promisse all

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-axios
-    .get('https://api.github.com/users/SaimonDavi')
-    .then(res => {
-        const user = res.data
-
-        axios.get(user.repos_url)
-            .then(repos => console.log(repos.data))
-    })
-    .catch(error => {
-        console.log(error)
-    })
+Promise.all([
+    axios.get('https://api.github.com/users/SaimonDavi'),
+    axios.get('https://api.github.com/users/SaimonDavi/repos')
+])
+.then( responses => {
+    console.log(responses[0].data.login)
+    console.log(responses[1].data.length)
+})
+.catch( err => console.log(err.message))
